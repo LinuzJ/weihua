455543066d47f26561c16ff1600e2cbb5d1b5016
Markus Nyman <markus@nyman.dev>
2023-11-11 18:50:05 +0200

    send video to yolo

--- backend/pocketbase/cmd/main.go
+++ backend/pocketbase/cmd/main.go
@@ -1,35 +1,55 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 
-	"github.com/LinuzJ/weihua/score"
-	"github.com/labstack/echo/v5"
 	"github.com/pocketbase/pocketbase"
-	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/models"
 )
 
 func main() {
 	app := pocketbase.New()
 
-	// serves static files from the provided public dir (if exists)
-	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
-		e.Router.GET("/api/weihua/score", func(c echo.Context) error {
+	app.OnRecordAfterCreateRequest("videos").Add(func(e *core.RecordCreateEvent) error {
+		videoFile := e.Record.Get("video").(string)
 
-			// Figure out where video is
-			// Get reference score
+		go getScore(app, e.Record, videoFile)
 
-			// GetScore
-			score := score.GetScore()
-
-			return c.String(http.StatusOK, score)
-		}, apis.ActivityLogger(app))
 		return nil
 	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getScore(app *pocketbase.PocketBase, record *models.Record, fileName string) {
+	log.Printf("creating score for video, %s\n", record.Id)
+
+	yoloServerUrl := fmt.Sprintf("http://127.0.0.1:5000/infer?video=https://127.0.0.1:8080/api/files/videos/%s/%s", record.Id, fileName)
+
+	response, err := http.Get(yoloServerUrl)
+	if err != nil {
+		fmt.Printf("Error making GET request: %v\n", err)
+		return
+	}
+	defer response.Body.Close()
+
+	// Read the response body
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error reading response body: %v\n", err)
+		return
+	}
+
+	record.Set("infer", body)
+	if err := app.Dao().SaveRecord(record); err != nil {
+		log.Printf("failed to save error %v\n", err)
+	}
+	// form-data with frames1 and frames2
+	// yoloCompare := "http://127.0.0.1:5000/compare"
 }
