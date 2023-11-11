d0f6be38ec5fa304233595c9dd968b8d97f2ea29
Markus Nyman <markus@nyman.dev>
2023-11-11 20:37:23 +0200

    fix score calculation

--- backend/pocketbase/cmd/main.go
+++ backend/pocketbase/cmd/main.go
@@ -4,14 +4,12 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"net/http"
-
-	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
 	"github.com/pocketbase/pocketbase/models"
+	"io"
+	"log"
+	"net/http"
 )
 
 type Comparison struct {
@@ -23,51 +21,7 @@ func main() {
 
 	app.OnRecordAfterCreateRequest("videos").Add(func(e *core.RecordCreateEvent) error {
 		videoFile := e.Record.Get("video").(string)
-
-		go infer(app, e.Record, videoFile)
-
-		return nil
-	})
-
-	app.OnRecordAfterUpdateRequest("videos").Add(func(e *core.RecordUpdateEvent) error {
-		infer := e.Record.Get("infer")
-		if infer == nil {
-			return nil
-		}
-
-		log.Printf("calculating score for video: %s", e.Record.Id)
-
-		reqBody, err := json.Marshal(Comparison{
-			comparison1: infer.(string),
-			comparison2: infer.(string),
-		})
-		if err != nil {
-			return fmt.Errorf("comparison to json: %v", err)
-		}
-
-		res, err := http.Post("http://127.0.0.1:5000/compare", "application/json", bytes.NewBuffer(reqBody))
-		if err != nil {
-			return fmt.Errorf("comparison: %v", err)
-		}
-		defer res.Body.Close()
-
-		scoreRecords, err := app.Dao().FindRecordsByFilter(
-			"scores",
-			"video = {:video}",
-			"",
-			1,
-			0,
-			dbx.Params{"video": e.Record.Id},
-		)
-
-		body, _ := io.ReadAll(res.Body)
-
-		scoreRecord := scoreRecords[0]
-		scoreRecord.Set("score", body)
-		if err := app.Dao().SaveRecord(scoreRecord); err != nil {
-			return fmt.Errorf("saving score: %v", err)
-		}
-
+		go inferAndScore(app, e.Record, videoFile)
 		return nil
 	})
 
@@ -76,7 +30,7 @@ func main() {
 	}
 }
 
-func infer(app *pocketbase.PocketBase, record *models.Record, fileName string) {
+func inferAndScore(app *pocketbase.PocketBase, record *models.Record, fileName string) {
 	log.Printf("creating infer for video: %s\n", record.Id)
 
 	inferUrl := fmt.Sprintf("http://127.0.0.1:5000/infer?video=http://127.0.0.1:8080/api/files/videos/%s/%s", record.Id, fileName)
@@ -97,5 +51,31 @@ func infer(app *pocketbase.PocketBase, record *models.Record, fileName string) {
 	record.Set("infer", body)
 	if err := app.Dao().SaveRecord(record); err != nil {
 		log.Printf("failed to save error %v\n", err)
+	}
+	log.Printf("saved infer for: %s", record.Id)
+
+	log.Printf("calculating score for video: %s", record.Id)
+	reqBody, err := json.Marshal(Comparison{
+		comparison1: string(body),
+		comparison2: string(body),
+	})
+	if err != nil {
+		log.Printf("comparison to json failed: %v", err)
+		return
+	}
+
+	res, err := http.Post("http://127.0.0.1:5000/compare", "application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		log.Printf("comparison failed: %v", err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, _ = io.ReadAll(res.Body)
+	record.Set("score", body)
+
+	if err := app.Dao().SaveRecord(record); err != nil {
+		log.Printf("saving score failed: %v", err)
+		return
 	}
 }
