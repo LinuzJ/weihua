2527563cbd640f34bd4d05ebcd4faaa01230c558
Peik Etzell <peik.etzell@gmail.com>
2023-11-11 19:30:23 +0200

    refactor comparison

--- backend/yolo_model/comparison.py
+++ backend/yolo_model/comparison.py
@@ -7,8 +7,9 @@ from dtaidistance import dtw
 # call only this one!
 def score(frames1, frames2):
     # high means good match
-    return 100 * (1 - _distance(_frames_to_movements(frames1),
-                                _frames_to_movements(frames2)))
+    return 100 * (
+        1 - _distance(_frames_to_movements(frames1), _frames_to_movements(frames2))
+    )
 
 
 def _frames_to_movements(frames):
@@ -16,28 +17,31 @@ def _frames_to_movements(frames):
     # assert len(np.asarray(frames).shape) == 1, 'Take only one person for each frame'
     # all relative to upper left corner of the bounding box
     movements = [[] for _ in range(0, 17)]
-    for frame in frames:
-        frame = frame[0]  # first person in the frame
-        for i in range(0, 17):
-            x = frame['keypoints']['x'][i]
-            if x != 0:
-                x -= frame['box']['x1']
+    for frame in [f[0] for f in frames]:  # first person in the frame
+        box = frame["box"]
+        box_center_x = (box["x2"] - box["x1"]) / 2
+        box_center_y = (box["y2"] - box["y1"]) / 2
 
-            y = frame['keypoints']['y'][i]
+        x_series = frame["keypoints"]["x"]
+        y_series = frame["keypoints"]["y"]
+        for i in range(0, 17):
+            x = x_series[i]
+            if x != 0:
+                x -= box_center_x
+
+            y = x_series[i]
             if y != 0:
-                y -= frame['box']['y1']
+                y -= box_center_y
 
             movements[i].append((x, y))
     movements = np.asarray(movements)
 
     for i in range(0, 17):
         if np.linalg.norm(movements[i, :, 0]) != 0:
-            movements[i, :, 0] = movements[i, :, 0] / \
-                np.linalg.norm(movements[i, :, 0])
+            movements[i, :, 0] = movements[i, :, 0] / np.linalg.norm(movements[i, :, 0])
 
         if np.linalg.norm(movements[i, :, 1]) != 0:
-            movements[i, :, 1] = movements[i, :, 1] / \
-                np.linalg.norm(movements[i, :, 1])
+            movements[i, :, 1] = movements[i, :, 1] / np.linalg.norm(movements[i, :, 1])
     return movements
 
 
