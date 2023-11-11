733c3e70cfc0871b2f07df80933319ac1e2ab336
Ilia <ilia.zalesskii@protonmail.com>
2023-11-11 13:39:58 +0200

    movement comparison python file

---
+++ backend/yolo_model/comparison.py
@@ -0,0 +1,45 @@
+import numpy as np
+from dtaidistance import dtw
+
+
+# call only this one!
+def score(frames1, frames2):
+    # high means good match
+    return 100 * (1 - _distance(_frames_to_movements(frames1),
+                                _frames_to_movements(frames2)))
+
+
+def _frames_to_movements(frames):
+    assert len(np.asarray(frames).shape) == 1, 'Take only one person for each frame'
+    movements = [[] for i in range(0, 17)]  # all relative to upper left corner of the bounding box
+    for frame in frames:
+        for i in range(0, 17):
+            x = frame['keypoints']['x'][i]
+            if x != 0:
+                x -= frame['box']['x1']
+
+            y = frame['keypoints']['y'][i]
+            if y != 0:
+                y -= frame['box']['y1']
+
+            movements[i].append((x, y))
+    movements = np.asarray(movements)
+
+    for i in range(0, 17):
+        if np.linalg.norm(movements[i, :, 0]) != 0:
+            movements[i, :, 0] = movements[i, :, 0] / np.linalg.norm(movements[i, :, 0])
+
+        if np.linalg.norm(movements[i, :, 1]) != 0:
+            movements[i, :, 1] = movements[i, :, 1] / np.linalg.norm(movements[i, :, 1])
+    return movements
+
+
+def _distance(move1, move2):
+    dists = []
+    for i in range(0, 17):
+        for j in range(0, 2):
+            dist = dtw.distance(move1[i, :, j], move2[i, :, j])
+            dists.append(dist)
+    # print(dists)
+
+    return np.mean(dists)
